cfca99986a4cc0be162e5779e54e780bccd4a3c5
Morgan Sennhauser <emsenn@emsenn.com>
2011-04-20 10:41:15 -0400

    changed the main dev readme to point at git/github/irc

--- lib/std/room.py
+++ lib/std/room.py
@@ -8,10 +8,16 @@ emsenn@Stirling 190411
 from stirling.lib.std.object import MasterObject
 
 class Room(MasterObject):
-    def __init__(self):
+    def def__init__(self):
         super(Room, self).__init__()
-
+        self.exits = []
     def write(self, data):
+        """
+        write(text)
+        
+        Outputs the string text to everything in the room's inventory.
+        """
+        # Need to add a link to the microsyntax interpretter when we have it.
         for item in self.inventory:
             item.tell(data+'\n')
-
+    
