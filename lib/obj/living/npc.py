6c00a8fe96c674eae2868dbc5092c90f52b81bd7
Morgan Sennhauser <emsenn@emsenn.com>
2011-04-25 10:37:29 -0400

    Added example NPC and NPC inheritable

---
+++ lib/obj/living/npc.py
@@ -0,0 +1,17 @@
+"""
+/obj/living/npc.py
+emsenn@Stirling
+250411
+
+    Inheritable for NPCs.  Doesn't do anything, but we're going to need it so 
+*shrug*.
+"""
+
+from stirling.lib.obj.living.living import Living
+
+class NPC(Living):
+    def __init__(self):
+        super(NPC, self).__init__()
+
+    def tell(self, data):
+        super(NPC, self).tell(data)
