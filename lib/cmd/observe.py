68202813b769f5ab417a655e490fb0d920199085
Morgan Sennhauser <emsenn@emsenn.com>
2011-04-27 11:45:12 -0400

    Showing abzde my failure

---
+++ lib/cmd/observe.py
@@ -0,0 +1,11 @@
+
+def do_observe(obj, target="here"):
+    '''
+    usage: observe [target]
+        returns the description of the target, defaulting to current environment
+    '''
+    if target is "here":
+        obj.tell(obj.environment.desc())
+    else:
+        obj.tell("Sorry, this command can't handle arguments yet")
+
