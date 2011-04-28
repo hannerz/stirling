347fe62bdbe17b366b4cb8852e5378ce3b0e5306
Morgan Sennhauser <emsenn@emsenn.com>
2011-04-28 14:30:33 -0400

    Added alot of yetis, in response to issue #3

---
+++ world/dev/npc/alot_of_rubber.py
@@ -0,0 +1,16 @@
+'''
+/world/dev/room/alot.py
+emsenn@Stirling
+250411
+
+Part of the test suite, this is an alot.
+'''
+
+from stirling.lib.obj.living.npc import NPC
+
+class Alot(NPC):
+    def __init__(self):
+        super(Alot, self).__init__()
+        self.set_name('alot of rubber')
+        self.set_desc('This squeaky fellow is an alot of rubber.  Matte black '
+          'eyes are inset in his smooth polymer body.')
