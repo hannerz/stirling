3e9288c42fa7f80e1104fc2f1f0f344db7092829
hannerz <hannerz@emsenn.com>
2011-04-28 19:15:12 -0700

    cafe to python

--- hannerzgit/cafe.py
+++
@@ -1,8 +0,0 @@
-from stirling.lib.obj.room import Room
-
-class Cafe(Room):
-    def __init__(self):
-        super(Cafe, self).__init__()
-        self.set_name('cafe')
-        self.set_desc('This is a cafe.')
-#       self.set_exits(['east':'stirling.world.dev.room.courtyard'])
